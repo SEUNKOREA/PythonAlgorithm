bbe8124ac654502e90d4d52c0f5ec9e1c5bd88b6
leeseeun <lse1002@hanyang.ac.kr>
2024-01-14 22:44:19 +0900

    Dynamic Programming

---
+++ Programmers/가사 검색.py
@@ -0,0 +1,34 @@
+from bisect import bisect_left, bisect_right
+
+def count_by_range(a, left_value, right_value):
+    right_index = bisect_right(a, right_value)
+    left_index = bisect_left(a, left_value)
+    return right_index - left_index
+
+# 모든 단어를 길이마다 나누어서 저장하기 위한 리스트
+array = [[] for _ in range(10001)]
+# 모든 단어를 길이마다 나누어서 저장하기 위한 리스트
+reversed_array = [[] for _ in range(10001)]
+
+def solution(words, queries):
+    answer = []
+    for word in words:
+        array[len(word)].append(word)
+        reversed_array[len(word)].append(word[::-1])
+
+    for i in range(10001):
+        array[i].sort()
+        reversed_array[i].sort()
+    
+    for query in queries:
+        if query[0] != "?":
+            res = count_by_range(array[len(query)], query.replace("?", "a"), query.replace("?", "z"))
+        else:
+            res = count_by_range(reversed_array[len(query)], query[::-1].replace("?", "a"), query[::-1].replace("?", "z"))
+        answer.append(res)
+    return answer
+
+
+words = ["frodo", "front", "frost", "frozen", "frame", "kakao"]
+queries = ["fro??", "????o", "fr???", "fro???", "pro?"]
+print(solution(words, queries))
