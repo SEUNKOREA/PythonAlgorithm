496cf28ae1761deee5492686607dee9e7aac59b8
leeseeun <lse1002@hanyang.ac.kr>
2024-03-25 23:34:23 +0900

    20240325

--- swea/swea_5653.py
+++ swea/swea_5653.py
@@ -2,29 +2,68 @@
 5653. [모의 SW 역량테스트] 줄기세포배양
 출처: https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AWXRJ8EKe48DFAUo
 """
-from collections import deque
-
 import sys
-sys.stdin = open("input.txt", "r")
+sys.stdin = open('input.txt', 'r')
+class cell:
+    def __init__(self, x, y, time):
+        self.hp=x # 비활성화에서 활성화까지 남은시간 혹은 활성화에서 죽을 때까지 시간
+        self.ahp=x # 활성화하는데 걸리는 시간
+        self.status=y # 1 비활성, 2 활성, 3 죽음, 0 없음
+        self.time=time # 생성된 시간
 
-dx = [-1, 1, 0, 0]
-dy = [0, 0, -1, 1]
+def spread(x, y, w, t):
+    dx = [-1, 1, 0, 0]
+    dy = [0, 0, -1, 1]
 
-for tc in range(1, int(input())+1):
-    n, m, k = map(int, input().split()) # 세로, 가로, 배양시간
+    for i in range(4):
+        nx = x + dx[i]
+        ny = y + dy[i]
+        if map1[nx][ny].status == 0:
+            map1[nx][ny].status = 1
+            map1[nx][ny].hp = w
+            map1[nx][ny].ahp = w
+            map1[nx][ny].time = t
+        elif map1[nx][ny].status == 1 and map1[nx][ny].time == t:
+            if w > map1[nx][ny].hp:
+                map1[nx][ny].hp = w
+                map1[nx][ny].ahp = w
 
-    q = deque()
 
-    array = []
+T = int(input())
+for tc in range(1, T+1):
+    n, m, k = map(int, input().split())
+    current_time = 0
+    map1 = [[cell(0, 0, 0) for _ in range(m+k)] for _ in range(n+k)]
+    map2 = [list(map(int, input().split())) for _ in range(n)]
     for i in range(n):
-        array.append(list(map(int, input().split())))
         for j in range(m):
-            if array[i][j] != 0:
-                q.append((0, i, j)) # 생성된 시간, 좌표
+            if map2[i][j] != 0:
+                map1[i + k // 2][j + k // 2].status = 1
+                map1[i + k // 2][j + k // 2].hp = map2[i][j]
+                map1[i + k // 2][j + k // 2].ahp = map2[i][j]
 
-    while q:
-        time, x, y = q.popleft()
-        power = array[x][y]
+    sero = len(map1)
+    garo = len(map1[0])
+    while current_time < k:
+        current_time += 1
+        for x in range(sero):
+            for y in range(garo):
+                if map1[x][y].status == 1 and map1[x][y].time < current_time:
+                    map1[x][y].hp -= 1
+                    if map1[x][y].hp == 0:
+                        map1[x][y].status = 2
+                        map1[x][y].hp = map1[x][y].ahp
+                        continue
+                elif map1[x][y].status == 2:
+                    map1[x][y].hp -= 1
+                    if map1[x][y].hp == map1[x][y].ahp-1:
+                        spread(x, y, map1[x][y].ahp, current_time)
+                    if map1[x][y].hp == 0:
+                        map1[x][y].status = 3
+    answer = 0
+    for i in range(sero):
+        for j in range(garo):
+            if map1[i][j].status == 1 or map1[i][j].status == 2:
+                answer += 1
 
-        for i in range(4):
-
+    print(f"#{tc} {answer}")
